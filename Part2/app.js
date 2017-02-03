52d0069485ea2e20b605c4303f6a53ddd2eeef3d
hwray <hmcwray@gmail.com>
2017-02-03 00:11:39 -0800

    Added Socket.io for networking, as well as example chat app from official tutorial.

--- Part2/app.js
+++ Part2/app.js
@@ -1,10 +1,30 @@
-var express = require( 'express' );
-var app = express();
+var app = require( 'express' )();
+var http = require( 'http' ).Server( app );
+var io = require( 'socket.io' )( http );
+
 
 app.get( '/', function( req, res ) {
-  res.send( 'Hello World!' )
+	
+	res.sendFile( __dirname + '/index.html' );
 });
 
-app.listen( 3000, function() {
-  console.log( 'Example app listening on port 3000!' )
+
+io.on( 'connection', function( socket ) {
+
+	console.log( 'a user connected' );
+
+	socket.on( 'chat message' , function( msg ) {
+		console.log( msg ); 
+		io.emit( 'chat message', msg );
+	});
+
+	socket.on( 'disconnect', function() {
+		console.log( 'user disconnected' );
+	});
+});
+
+
+http.listen( 3000, function() {
+
+	console.log( 'App listening on *:3000' );
 });
